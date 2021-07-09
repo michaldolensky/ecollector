0bf0a88a6bc6950f8fc32e999cc69af8d24f4d65
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-07-09 22:03:13 +0200

    chore(api): add nest config, typeorm, pg

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const HOSTNAME = process.env.HOSTNAME;
+const PORT = process.env.PORT;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
+  console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
 }
 bootstrap();
