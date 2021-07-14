19ecadde0a5ed15e253e9de665a89ba610749db0
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-07-14 21:23:20 +0200

    refactor(api): remove auth.controller

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -3,7 +3,6 @@ import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
-import { AuthController } from './auth.controller';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 
