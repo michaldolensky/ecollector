b1d1f6110d211527d3dae4bff6b7750ff551459a
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-07-19 23:32:06 +0200

    feat(api): change console.log to logger in app.module

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,19 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 const HOSTNAME = process.env.HOSTNAME;
 const PORT = process.env.PORT;
 // const NODE_ENV = process.env.NODE_ENV;
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
   //TODO:
   // if (NODE_ENV !== 'developement') app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   await app.listen(PORT);
-  console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+
+  logger.log(`Server running at http://${HOSTNAME}:${PORT}/`);
 }
 bootstrap();
