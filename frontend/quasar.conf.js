65955b6150a480401ca5bf4df86a648f973c8054
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-07-21 00:32:55 +0200

    chore(frontend): enable material-icons

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -92,7 +92,7 @@ module.exports = configure((ctx) => ({
   framework: {
     config: {},
 
-    // iconSet: 'material-icons', // Quasar icon set
+    iconSet: 'material-icons', // Quasar icon set
     // lang: 'en-US', // Quasar language pack
 
     // For special cases outside of where the auto-import strategy can have an impact
