974dd2dfe2441b7715b33220ad0367c0fcd913e1
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-07-21 00:22:07 +0200

    feat(api): add LoginInterface

---
+++ api/src/auth/dto/login.dto.ts
@@ -0,0 +1,9 @@
+export interface LoginInterface {
+  email: string;
+  password: string;
+}
+
+// export class LoginDto implements LoginInterface {
+//   email: string;
+//   password: string;
+// }
--- api/src/auth/dto/login.input.ts
+++
@@ -1,5 +0,0 @@
-export class LoginInput {
-  email: string;
-
-  password: string;
-}
