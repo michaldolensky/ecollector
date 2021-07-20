f7f8b32d236c4f44b3c7f9808cb6b596097343fb
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-07-21 00:21:55 +0200

    feat(api): enable cors for dev url of frontend

--- api/src/main.ts
+++ api/src/main.ts
@@ -11,7 +11,10 @@ async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
   //TODO:
-  // if (NODE_ENV !== 'developement') app.enableCors();
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:8080'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   await app.listen(PORT);
