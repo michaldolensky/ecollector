50331c4fcea3861c00a4aa3f3aea7783d3a41662
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-07-23 21:37:18 +0200

    feat(frontend): add DashboardNavigationItems object

---
+++ frontend/src/data/DashboardNavigationItems.ts
@@ -0,0 +1,9 @@
+import { NavigationItemInterface } from 'src/types/NavigationItem.interface';
+
+export const DashboardNavigationItems:NavigationItemInterface[] = [
+  {
+    name: 'dashboard.navigation.home',
+    icon: 'home',
+    url: '',
+  },
+];
