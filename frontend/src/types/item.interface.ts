70fc161a977dca5fac3ae7a9050ac442cca68410
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-07-25 02:19:18 +0200

    feat(frontend): update item interface

--- frontend/src/types/item.interface.ts
+++ frontend/src/types/item.interface.ts
@@ -1,4 +1,9 @@
+import { CategoryInterface } from 'src/types/category.interface';
+
 export interface ItemInterface {
   id:number
   name:string
+  updatedAt:Date
+  createdAt:Date
+  category:CategoryInterface
 }
