caf49fc8a2f2312178a93ad9315840e83e86c729
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-07-26 20:55:29 +0200

    feat(api): add typeorm config

---
+++ api/ormconfig.ts
@@ -0,0 +1,23 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+console.log(process.env.POSTGRES_HOST);
+export = {
+  type: 'postgres' as const,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: ['src/**/entities/*.entity.ts'],
+  migrations: ['src/migrations/*.{ts,js}'],
+  cli: {
+    migrationsDir: 'src/migrations/',
+  },
+  extra: {
+    ssl:
+      process.env.NODE_ENV === 'production'
+        ? { rejectUnauthorized: false }
+        : false,
+  },
+};
