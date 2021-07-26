d81a3f3718acea379489385aeefe7e114fe72f9c
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-07-26 02:55:44 +0200

    chore(frontend): disable eslint no-console while in debug mode

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -92,6 +92,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
 
     // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
