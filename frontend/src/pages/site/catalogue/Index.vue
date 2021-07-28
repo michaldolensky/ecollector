f618c2441b3a2ba29a0564402815d38224653486
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-07-28 22:50:06 +0200

    feat(frontend): add pages for catalogue Index,Item,Items

---
+++ frontend/src/pages/site/catalogue/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-layout container>
+    <p
+      v-for="index in 10 "
+      :key="index"
+      class="text-black"
+    >
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
+
+      blanditiis culpa, cum debitis dignissimos eaque est explicabo impedit iusto
+      maiores quo ratione rem saepe. Ab
+      debitis dolorum libero porro praesentium.
+    </p>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  // name: 'PageName'
+});
+</script>
