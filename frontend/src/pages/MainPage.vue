3651e5d51bbc45795ab8b49b75c29facfadca315
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-08-21 13:47:05 +0200

    feat(frontend): disable MainPage router link

--- frontend/src/pages/MainPage.vue
+++ frontend/src/pages/MainPage.vue
@@ -1,34 +1,12 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <q-list>
-      <q-list>
-        >
-        <template
-          v-for="route in allRoutes"
-          :key="route.path"
-        >
-          <EssentialLink
-            :link="route.path"
-            :title="route.path"
-          />
-        </template>
-      </q-list>
-    </q-list>
-  </q-page>
+  <q-page class="row items-center justify-evenly" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { useRouter } from 'vue-router';
-import EssentialLink from 'components/EssentialLink.vue';
 
 export default defineComponent({
   name: 'PageIndex',
-  components: { EssentialLink },
-  setup() {
-    const $router = useRouter();
-    const allRoutes = $router.getRoutes();
-    return { allRoutes };
-  },
+  components: { },
 });
 </script>
