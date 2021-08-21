ac3eaa6fb953fadf4f1e80df174283580f3ff7a0
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-08-22 00:01:48 +0200

    refactor(frontend): rename parameter in getParsedInt

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -3,4 +3,4 @@ import { Quasar } from 'quasar';
 // eslint-disable-next-line max-len
 export const toRowDate = (dateString:string) => new Intl.DateTimeFormat(Quasar.lang.getLocale()).format(new Date(dateString));
 // eslint-disable-next-line max-len
-export const getParsedInt = (siteIdParam: string|string[]):number => parseInt(<string>siteIdParam, 10);
+export const getParsedInt = (paramId: string|string[]):number => parseInt(<string>paramId, 10);
