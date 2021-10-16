b60715d3a799ad00d20a2b0e0a662e50d7bdf50a
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-10-16 20:37:41 +0200

    refactor(frontend): convert to apollo-vue from @quasar/apollo

--- frontend/src/boot/apollo.ts
+++ frontend/src/boot/apollo.ts
@@ -1,17 +1,30 @@
-import { ApolloClient } from '@apollo/client/core';
-import { ApolloClients } from '@vue/apollo-composable';
+import {
+  ApolloClient, createHttpLink, InMemoryCache,
+} from '@apollo/client/core';
+import { DefaultApolloClient } from '@vue/apollo-composable';
 import { boot } from 'quasar/wrappers';
-import { getClientOptions } from 'src/apollo';
+import { localStorageTokenKey } from 'src/stores/auth';
+
+const token = localStorage.getItem(localStorageTokenKey) ?? '';
 
 export default boot(
   ({ app }) => {
-    const options = getClientOptions();
-    const apolloClient = new ApolloClient(options);
+    const apolloClient = new ApolloClient({
+      link: createHttpLink({
+        credentials: 'include',
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+        uri:
+          process.env.GRAPHQL_URI
+          || 'http://localhost:3000/api/graphql',
+      }),
+      cache: new InMemoryCache({
+        addTypename: false,
+      }),
+      connectToDevTools: process.env.NODE_ENV === 'development',
+    });
 
-    const apolloClients: Record<string, ApolloClient<unknown>> = {
-      default: apolloClient,
-    };
-
-    app.provide(ApolloClients, apolloClients);
+    app.provide(DefaultApolloClient, apolloClient);
   },
 );
