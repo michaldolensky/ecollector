34a7d22fd68ea93549c3efe5e41b2910b6f95a25
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-11-05 22:04:08 +0100

    refactor(frontend): convert EditItemPage to script setup

--- frontend/src/pages/site/dashboard/items/EditItemPage.vue
+++ frontend/src/pages/site/dashboard/items/EditItemPage.vue
@@ -12,63 +12,52 @@
   </q-page>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import ItemForm from 'components/dashboard/forms/ItemForm.vue';
 import { useQuasar } from 'quasar';
 import { ItemInput, useItems } from 'src/module/useItems';
-import { defineComponent, onMounted, ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
-export default defineComponent({
-  name: 'ItemDashboard',
-  components: { ItemForm },
-  setup() {
-    const route = useRoute();
-    const router = useRouter();
-    const editItem = ref();
-    const { createItem, updateItem, getItem } = useItems();
-    const $q = useQuasar();
+const route = useRoute();
+const router = useRouter();
+const editItem = ref();
+const { createItem, updateItem, getItem } = useItems();
+const $q = useQuasar();
 
-    const itemId = parseInt(<string>route.params.itemId, 10);
+const itemId = parseInt(<string>route.params.itemId, 10);
 
-    onMounted(() => {
-      if (route.name === 'DashBoardItemCreate') {
-        editItem.value = {};
-      } else {
-        const { onResult } = getItem(itemId);
-        onResult((result) => {
-          editItem.value = result.data.item;
+onMounted(() => {
+  if (route.name === 'DashBoardItemCreate') {
+    editItem.value = {};
+  } else {
+    const { onResult } = getItem(itemId);
+    onResult((result) => {
+      editItem.value = result.data.item;
+    });
+  }
+});
+const handleSubmit = (object:ItemInput) => {
+  if (object.id) {
+    void updateItem(object).then((result) => {
+      if (result?.data) {
+        $q.notify({
+          type: 'positive',
+          message: 'Item updated',
         });
+        editItem.value = result.data.updateItem;
       }
     });
-    const handleSubmit = (object:ItemInput) => {
-      if (object.id) {
-        void updateItem(object).then((result) => {
-          if (result?.data) {
-            $q.notify({
-              type: 'positive',
-              message: 'Item updated',
-            });
-            editItem.value = result.data.updateItem;
-          }
+  } else {
+    void createItem(object).then((data) => {
+      if (data?.data) {
+        $q.notify({
+          type: 'positive',
+          message: 'Item created',
         });
-      } else {
-        void createItem(object).then((data) => {
-          if (data?.data) {
-            $q.notify({
-              type: 'positive',
-              message: 'Item created',
-            });
-            void router.push({ name: 'DashBoardItemEdit', params: { itemId: data.data.createItem.id } });
-          }
-        });
+        void router.push({ name: 'DashBoardItemEdit', params: { itemId: data.data.createItem.id } });
       }
-    };
-
-    return {
-      handleSubmit,
-      editItem,
-    };
-  },
-});
+    });
+  }
+};
 </script>
