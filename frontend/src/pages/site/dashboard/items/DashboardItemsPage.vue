5ce39bf74604b8c30317825377c6854fa4b51631
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-11-05 21:36:04 +0100

    feat(frontend): modify DashboardItemsPage layout add search card add heading with name replace floating button wih proper in heading

--- frontend/src/pages/site/dashboard/items/DashboardItemsPage.vue
+++ frontend/src/pages/site/dashboard/items/DashboardItemsPage.vue
@@ -1,37 +1,51 @@
 <template>
   <q-page
+    class="q-pa-md col items-start q-gutter-md"
     padding
   >
-    <div v-if="loading">
-      Loading...
+    <div>
+      <span class="text-h4 ">Items</span>
+      <div class="float-right">
+        <q-btn
+          :to="{name:'DashBoardItemCreate'}"
+          color="primary"
+          icon="add"
+          label="Add item"
+        />
+      </div>
     </div>
-    <div
-      v-else-if="true"
-      class="q-pa-md"
-    >
+
+    <q-card class="q-pa-md">
+      <q-input
+        v-model="filter"
+        debounce="300"
+        placeholder="Search"
+        stack-label
+      >
+        <template #append>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+    </q-card>
+
+    <q-card v-if="loading">
+      <items-table :loading="loading" />
+    </q-card>
+    <q-card v-else>
       <items-table
+        :filter="filter"
         :items="result.items"
-        :loading="loading"
       />
-    </div>
-    <q-page-sticky
-      :offset="[18, 18]"
-      position="bottom-right"
-    >
-      <q-btn
-        :to="{name:'DashBoardItemCreate'}"
-        color="accent"
-        fab
-        icon="add"
-      />
-    </q-page-sticky>
+    </q-card>
   </q-page>
 </template>
 
 <script lang="ts" setup>
-import { useItems } from 'src/module/useItems';
 import ItemsTable from 'components/dashboard/tables/ItemsTable.vue';
+import { useItems } from 'src/module/useItems';
+import { ref } from 'vue';
 
 const { result, loading } = useItems();
+const filter = ref('');
 
 </script>
