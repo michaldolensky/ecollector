a237531b4e6663497883030de700b006fdf236be
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-11-06 00:53:56 +0100

    fix(frontend): getCategories not callable

--- frontend/src/module/useCategories.ts
+++ frontend/src/module/useCategories.ts
@@ -24,7 +24,7 @@ export interface CategoryInput{
 export function useCategories() {
   const { currentSiteId } = useSites();
 
-  const getCategories = useGetCategoriesQuery({
+  const getCategories = () => useGetCategoriesQuery({
     siteId: currentSiteId.value,
   });
 
