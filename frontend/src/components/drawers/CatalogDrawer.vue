b4e939a079e9232cecbf5f5106981f423c5c122b
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-11-13 01:31:44 +0100

    fix(frontend): move header to outside of virtual scroll CatalogDrawer

--- frontend/src/components/drawers/CatalogDrawer.vue
+++ frontend/src/components/drawers/CatalogDrawer.vue
@@ -6,59 +6,52 @@
     elevated
     show-if-above
     side="left"
-    style="height: 100%"
   >
-    <q-scroll-area
-      id="scroll-area-with-virtual-scroll-1"
-      style="height: 100%"
-    >
-      <q-virtual-scroll
-        v-if="!loading"
-        :items="sortedCategories"
-        :virtual-scroll-item-size="32"
-        scroll-target="#scroll-area-with-virtual-scroll-1 > .scroll"
-        separator
+    <div style="flex: 0 1 auto;">
+      <q-item
+        v-ripple
+        :to="{name:'CatalogIndex'}"
+        clickable
+        exact
+        tag="a"
       >
-        <template #before>
-          <q-item
-            v-ripple
-            :to="{name:'CatalogIndex'}"
-            clickable
-            exact
-            tag="a"
-          >
-            <q-item-section avatar>
-              <q-icon name="home" />
-            </q-item-section>
+        <q-item-section avatar>
+          <q-icon name="home" />
+        </q-item-section>
 
-            <q-item-section>
-              Home
-            </q-item-section>
-          </q-item>
-          <q-separator />
+        <q-item-section>
+          Home
+        </q-item-section>
+      </q-item>
+      <q-separator />
 
-          <q-input
-            v-model="filter"
-            :debounce="500"
-            clearable
-            label="Category search"
-          >
-            <template #prepend>
-              <q-icon name="search" />
-            </template>
-          </q-input>
-
-          <q-separator size="5px" />
+      <q-input
+        v-model="filter"
+        :debounce="500"
+        clearable
+        label="Category search"
+      >
+        <template #prepend>
+          <q-icon name="search" />
         </template>
+      </q-input>
 
-        <template #default="{ item, index }">
-          <catalog-list-category
-            :key="index"
-            :category="item"
-          />
-        </template>
-      </q-virtual-scroll>
-    </q-scroll-area>
+      <q-separator size="5px" />
+    </div>
+    <q-virtual-scroll
+      v-if="!loading"
+      :items="sortedCategories"
+      :virtual-scroll-item-size="32"
+      scroll-target="#scroll-area-with-virtual-scroll-1 > .scroll"
+      separator
+    >
+      <template #default="{ item, index }">
+        <catalog-list-category
+          :key="index"
+          :category="item"
+        />
+      </template>
+    </q-virtual-scroll>
   </q-drawer>
 </template>
 
