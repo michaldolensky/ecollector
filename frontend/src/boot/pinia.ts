78ae696e37fc7f93ebbc684e8eebd1477a9cd7c7
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-11-13 20:33:02 +0100

    feat(frontend): add router to pinia stores

--- frontend/src/boot/pinia.ts
+++ frontend/src/boot/pinia.ts
@@ -1,6 +1,9 @@
 import { createPinia } from 'pinia';
 import { boot } from 'quasar/wrappers';
 
-export default boot(({ app }) => {
-  app.use(createPinia());
+export default boot(({ app, router }) => {
+  const pinia = createPinia();
+  pinia.use(() => ({ router }));
+
+  app.use(pinia);
 });
