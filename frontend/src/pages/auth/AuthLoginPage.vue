08e2723452a8e698e5b9b0f70a0f77d39091d4bd
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-11-13 17:57:14 +0100

    revert(frontend): replace card forms back to separate forms

--- frontend/src/pages/auth/AuthLoginPage.vue
+++ frontend/src/pages/auth/AuthLoginPage.vue
@@ -1,57 +1,59 @@
 <template>
   <q-page
-    class="flex flex-center"
     padding
   >
-    <q-card
-      class="login-card"
-      square
-      tag="form"
+    <q-form
+      class="flex flex-center"
       @submit="onSubmit"
     >
-      <q-card-section class="text-center text-h2">
-        {{ $t('pages.auth.login.title') }}
-      </q-card-section>
-      <q-card-section>
-        <q-input
-          id="email"
-          v-model.trim="loginData.email"
-          :label="$t('forms.auth.email')"
-          :rules="[required]"
-          autocomplete="username"
-          autofocus
-          lazy-rules
-          outlined
-          required
-          square
-          type="email"
-        />
-        <q-input
-          id="password"
-          v-model="loginData.password"
-          :label="$t('forms.auth.password')"
-          :rules="[required]"
-          autocomplete="current-password"
-          outlined
-          required
-          square
-          type="password"
-        />
-      </q-card-section>
-      <q-card-actions align="around">
-        <q-btn
-          :label="$t('buttons.auth.signup')"
-          :to="{ name: 'signup' }"
-          color="primary"
-          flat
-        />
-        <q-btn
-          :label="$t('buttons.auth.login')"
-          color="primary"
-          type="submit"
-        />
-      </q-card-actions>
-    </q-card>
+      <q-card
+        class="login-card"
+        square
+      >
+        <q-card-section class="text-center text-h2">
+          {{ $t('pages.auth.login.title') }}
+        </q-card-section>
+        <q-card-section>
+          <q-input
+            id="email"
+            v-model.trim="loginData.email"
+            :label="$t('forms.auth.email')"
+            :rules="[required]"
+            autocomplete="username"
+            autofocus
+            lazy-rules
+            outlined
+            required
+            square
+            type="email"
+          />
+          <q-input
+            id="password"
+            v-model="loginData.password"
+            :label="$t('forms.auth.password')"
+            :rules="[required]"
+            autocomplete="current-password"
+            outlined
+            required
+            square
+            type="password"
+          />
+        </q-card-section>
+        <q-card-actions align="around">
+          <q-btn
+            :label="$t('buttons.auth.signup')"
+            :to="{ name: 'signup' }"
+            color="primary"
+            flat
+          />
+          <q-btn
+            :label="$t('buttons.auth.login')"
+            color="primary"
+            type="submit"
+          />
+        </q-card-actions>
+      </q-card>
+    </q-form>
   </q-page>
 </template>
 
@@ -74,12 +76,7 @@ const loginData = reactive<LoginInterface>({
 });
 
 const onSubmit = () => {
-  void authStore.login(loginData)
-    .then(
-      () => {
-        void router.push({ name: 'profile' });
-      },
-    );
+  void authStore.login(loginData);
 };
 
 </script>
--- frontend/src/pages/auth/AuthSignupPage.vue
+++ frontend/src/pages/auth/AuthSignupPage.vue
@@ -1,83 +1,86 @@
 <template>
   <q-page
-    class="flex flex-center"
+    padding
   >
-    <q-card
-      class="login-card"
-      square
-      tag="form"
+    <q-form
+      class="flex flex-center"
       @submit="onSubmit"
     >
-      <q-card-section class="text-center text-h2">
-        {{ $t('pages.auth.signup.title') }}
-      </q-card-section>
-      <q-card-section>
-        <q-input
-          v-model.trim="signupData.firstName"
-          :label="$t('forms.auth.first_name')"
-          :rules="[ required]"
-          autofocus
-          lazy-rules
-          outlined
-          required
-          square
-        />
-        <q-input
-          v-model.trim="signupData.lastName"
-          :label="$t('forms.auth.last_name')"
-          :rules="[ required]"
-          lazy-rules
-          outlined
-          required
-          square
-        />
-        <q-input
-          v-model.trim="signupData.email"
-          :label="$t('forms.auth.email')"
-          :rules="[ required]"
-          lazy-rules
-          outlined
-          required
-          square
-          type="email"
-        />
-        <q-input
-          v-model="signupData.password"
-          :label="$t('forms.auth.password')"
-          :rules="[ required]"
-          outlined
-          required
-          square
-          type="password"
-        />
-        <q-input
-          v-model="signupData.verifyPassword"
-          :label="$t('forms.auth.confirm_password')"
-          :rules="[required]"
-          outlined
-          required
-          square
-          type="password"
-        />
-        <q-separator />
-      </q-card-section>
-      <q-card-actions
-        align="around"
+      <q-card
+        class="login-card"
+        square
       >
-        <q-btn
-          :label="$t('buttons.auth.login')"
-          :to="{ name: 'login' }"
-          color="primary"
-          flat
-        />
-        <q-btn
-          :label="$t('buttons.auth.signup')"
-          class="q-px-xl q-py-xs"
-          color="primary"
-          type="submit"
-        />
-      </q-card-actions>
-    </q-card>
+        <q-card-section class="text-center text-h2">
+          {{ $t('pages.auth.signup.title') }}
+        </q-card-section>
+        <q-card-section>
+          <q-input
+            v-model.trim="signupData.firstName"
+            :label="$t('forms.auth.first_name')"
+            :rules="[ required]"
+            autofocus
+            lazy-rules
+            outlined
+            required
+            square
+          />
+          <q-input
+            v-model.trim="signupData.lastName"
+            :label="$t('forms.auth.last_name')"
+            :rules="[ required]"
+            lazy-rules
+            outlined
+            required
+            square
+          />
+          <q-input
+            v-model.trim="signupData.email"
+            :label="$t('forms.auth.email')"
+            :rules="[ required]"
+            lazy-rules
+            outlined
+            required
+            square
+            type="email"
+          />
+          <q-input
+            v-model="signupData.password"
+            :label="$t('forms.auth.password')"
+            :rules="[ required]"
+            outlined
+            required
+            square
+            type="password"
+          />
+          <q-input
+            v-model="signupData.verifyPassword"
+            :label="$t('forms.auth.confirm_password')"
+            :rules="[required]"
+            outlined
+            required
+            square
+            type="password"
+          />
+          <q-separator />
+        </q-card-section>
+        <q-card-actions
+          align="around"
+        >
+          <q-btn
+            :label="$t('buttons.auth.login')"
+            :to="{ name: 'login' }"
+            color="primary"
+            flat
+          />
+          <q-btn
+            :label="$t('buttons.auth.signup')"
+            class="q-px-xl q-py-xs"
+            color="primary"
+            type="submit"
+          />
+        </q-card-actions>
+      </q-card>
+    </q-form>
   </q-page>
 </template>
 
