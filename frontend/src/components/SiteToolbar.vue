e4246339530928359b34963c2a118b24635f244d
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-11-15 23:23:06 +0100

    feat(frontend): display site name

--- frontend/src/components/SiteToolbar.vue
+++ frontend/src/components/SiteToolbar.vue
@@ -1,8 +1,10 @@
 <template>
   <q-toolbar inset>
-    <q-toolbar-title shrink>
-      <!--      todo-->
-      SiteName
+    <q-toolbar-title
+      v-if="result"
+      shrink
+    >
+      {{ result.site.name }}
     </q-toolbar-title>
     <q-separator
       dark
@@ -13,11 +15,17 @@
     <q-tabs align="left">
       <q-route-tab
         :label="$t('catalog.navigation.tab.catalog')"
-        :to="{name:'CatalogIndex',params:{siteId:1}}"
+        :to="{name:'CatalogIndex',params:{siteId:currentSiteId}}"
       />
     </q-tabs>
   </q-toolbar>
 </template>
 <script lang="ts" setup>
+
+import { useSiteQuery } from 'src/apollo/composition-functions';
+import { useSites } from 'src/composables/useSites';
+
+const { currentSiteId } = useSites();
+const { result } = useSiteQuery({ id: currentSiteId.value });
 
 </script>
