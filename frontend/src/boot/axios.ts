ab4dc7483c4b7c6e33e0ada440f096ddd8758a9e
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-12-02 22:30:43 +0100

    build(frontend): fix docker compose production to use args

--- frontend/src/boot/axios.ts
+++ frontend/src/boot/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 axios.defaults.withCredentials = true;
-const api = axios.create({ baseURL: `${process.env.SERVER_URL_API}` });
+const api = axios.create({ baseURL: process.env.SERVER_URL_API });
 
 export { api };
