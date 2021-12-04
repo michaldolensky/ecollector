6f1ee6eb800b8a7bc9441686668e3a2a121f7d4b
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-12-04 23:05:08 +0100

    feat(frontend): add basic CatalogItemImagesBrowser component

---
+++ frontend/src/components/catalog/CatalogItemImagesBrowser.vue
@@ -0,0 +1,84 @@
+<template>
+  <q-carousel
+    v-model="slide"
+    :arrows="moreThanOneImage"
+    :fullscreen="fullscreen"
+    animated
+    control-color="primary"
+    height="250px"
+    infinite
+    swipeable
+  >
+    <q-carousel-slide
+      v-for="(image, index) in images"
+      :key="index"
+      :name="index"
+    >
+      <q-img
+        :src="image.path"
+        class="rounded-borders col-6 full-height"
+        fit="contain"
+        max-height="100%"
+        max-width="100%"
+      />
+    </q-carousel-slide>
+
+    <template #control>
+      <q-carousel-control
+        :offset="[18, 18]"
+        position="bottom-right"
+      >
+        <q-btn
+          :icon="fullscreen ? 'fullscreen_exit' : 'fullscreen'"
+          color="white"
+          dense
+          push
+          round
+          text-color="primary"
+          @click="fullscreen = !fullscreen"
+        />
+      </q-carousel-control>
+    </template>
+  </q-carousel>
+  <q-virtual-scroll
+    v-if="images.length > 1"
+    :items="images"
+    class="row no-wrap q-gutter-x-sm"
+    style="overflow-x: auto; overflow-y: visible;"
+    virtual-scroll-horizontal
+  >
+    <template #default="{ item, index }">
+      <q-img
+        :key="index"
+        :src="item.path"
+        fit="contain"
+        height="50px"
+        style="padding-right: 10px; padding-left: 10px"
+        width="50px"
+        @click="slide = index"
+      />
+    </template>
+  </q-virtual-scroll>
+</template>
+
+<script lang="ts" setup>
+
+import { toRefs } from '@vueuse/core';
+import { Image } from 'src/apollo/composition-functions';
+import { computed, ref } from 'vue';
+
+interface Props {
+  images: Image[]
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  images: () => [],
+});
+const { images } = toRefs(props);
+
+const moreThanOneImage = computed((): boolean => images.length > 1);
+
+const slide = ref(0);
+const fullscreen = ref(false);
+
+</script>
