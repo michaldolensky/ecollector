c822aa0a11fd1c0da0dd23bce59ed28d056e7c5a
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-12-04 23:03:52 +0100

    feat(frontend): add Breadcrums component

---
+++ frontend/src/components/catalog/Breadcrums.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-breadcrumbs>
+    <q-breadcrumbs-el
+      :label="$t('catalog.navigation.tab.catalog')"
+      :to="{ name: 'CatalogIndex', params: { itemId,siteId } }"
+    />
+    <q-breadcrumbs-el
+      :label="$t('breadcrumbs.catalog.itemDetail')"
+      :to="{ name: 'CatalogItemDetail', params: { itemId,siteId } }"
+    />
+  </q-breadcrumbs>
+</template>
+
+<script lang="ts" setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const { siteId, itemId } = route.params;
+
+</script>
