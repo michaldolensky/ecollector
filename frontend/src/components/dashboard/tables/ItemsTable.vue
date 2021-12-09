ffe59b0982f763bde3877fb7a0cc35bede3373e5
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-12-09 23:28:09 +0100

    refactor(frontend): move ItemsTable columns to useDashboardTableColumns
    Signed-off-by: Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>

--- frontend/src/components/dashboard/tables/ItemsTable.vue
+++ frontend/src/components/dashboard/tables/ItemsTable.vue
@@ -58,11 +58,11 @@
 </template>
 <script lang="ts" setup>
 import { useQuasar } from 'quasar';
-import { toRowDate } from 'src/utils';
-import { useI18n } from 'vue-i18n';
-import { Item, useItems } from 'src/composables/useItems';
-import { computed, reactive } from 'vue';
+import { useDashboardTableColumns } from 'src/composables/dashboard/useDashboardTableColumns';
 import { SERVER_URL } from 'src/composables/useEnv';
+import { Item, useItems } from 'src/composables/useItems';
+import { reactive } from 'vue';
+import { useI18n } from 'vue-i18n';
 
 interface Props {
   loading?: boolean
@@ -77,6 +77,7 @@ const props = withDefaults(defineProps<Props>(), {
 
 const { removeItem } = useItems();
 const { dialog } = useQuasar();
+const { ItemsTableColumns } = useDashboardTableColumns();
 const { t } = useI18n();
 
 const initialPagination = reactive({
@@ -96,70 +97,5 @@ const confirmDelete = (item: Item) => {
     if (item.id != null) void removeItem(item.id);
   });
 };
-
-const ItemsTableColumns = computed(() => [
-  {
-    name: 'image',
-    required: true,
-    label: t('dashboard.items.table.column.label.image'),
-    align: 'left',
-    field: (item:Item) => item.images,
-    format: (val:string) => `${val}`,
-    sortable: true,
-  },
-  {
-    name: 'name',
-    required: true,
-    label: t('dashboard.items.table.column.label.name'),
-    align: 'left',
-    field: (item:Item) => item.name,
-    format: (val:string) => `${val}`,
-    sortable: true,
-  },
-  {
-    name: 'Category',
-    required: true,
-    label: t('dashboard.items.table.column.label.category'),
-    align: 'left',
-    field: (item:Item) => item.category.name,
-    format: (val:string) => `${val}`,
-    sortable: true,
-  },
-  {
-    name: 'availableForExchange',
-    required: true,
-    label: t('dashboard.items.table.column.label.available_for_exchange'),
-    align: 'left',
-    field: (item:Item) => item.numberForExchange,
-    format: (val:string) => `${val}`,
-    sortable: true,
-  },
-  {
-    name: 'count',
-    required: true,
-    label: t('dashboard.items.table.column.label.count'),
-    align: 'left',
-    field: (item:Item) => item.numberInCollection,
-    format: (val:string) => `${val}`,
-    sortable: true,
-  },
-  {
-    name: 'created',
-    required: true,
-    label: t('dashboard.items.table.column.label.created'),
-    align: 'left',
-    field: (item:Item) => item.createdAt,
-    format: (val:string) => `${toRowDate(val)}`,
-    sortable: true,
-  },
-  {
-    name: 'action',
-    label: t('dashboard.items.table.column.label.action'),
-    field: 'Action',
-    sortable: false,
-    align: 'center',
-  },
-
-]);
 
 </script>
---
+++ frontend/src/composables/dashboard/useDashboardTableColumns.ts
@@ -0,0 +1,82 @@
+import { Item } from 'src/composables/useItems';
+import { toRowDate } from 'src/utils';
+import { computed } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+export function useDashboardTableColumns() {
+  const { t } = useI18n();
+
+  const ItemsTableColumns = computed(() => [
+    {
+      name: 'image',
+      required: true,
+      label: t('dashboard.items.table.column.label.image'),
+      align: 'left',
+      field: (item:Item) => item.images,
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      format: (val:string) => val,
+      sortable: true,
+    },
+    {
+      name: 'name',
+      required: true,
+      label: t('dashboard.items.table.column.label.name'),
+      align: 'left',
+      field: (item:Item) => item.name,
+      format: (val:string) => `${val}`,
+      sortable: true,
+    },
+    {
+      name: 'Category',
+      required: true,
+      label: t('dashboard.items.table.column.label.category'),
+      align: 'left',
+      field: (item:Item) => item.category.name,
+      format: (val:string) => `${val}`,
+      sortable: true,
+    },
+    {
+      name: 'availableForExchange',
+      required: true,
+      label: t('dashboard.items.table.column.label.available_for_exchange'),
+      align: 'left',
+      field: (item:Item) => item.numberForExchange,
+      format: (val:string) => `${val}`,
+      sortable: true,
+    },
+    {
+      name: 'count',
+      required: true,
+      label: t('dashboard.items.table.column.label.count'),
+      align: 'left',
+      field: (item:Item) => item.numberInCollection,
+      format: (val:string) => `${val}`,
+      sortable: true,
+    },
+    {
+      name: 'created',
+      required: true,
+      label: t('dashboard.items.table.column.label.created'),
+      align: 'left',
+      field: (item:Item) => item.createdAt,
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+
+      format: (val:string) => `${toRowDate(val)}`,
+      sortable: true,
+    },
+    {
+      name: 'action',
+      label: t('dashboard.items.table.column.label.action'),
+      field: 'Action',
+      sortable: false,
+      align: 'center',
+    },
+
+  ]);
+
+  return {
+    ItemsTableColumns,
+  };
+}
