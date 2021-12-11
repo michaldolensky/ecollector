007974c26a268309e8589d9d48fa05b199fa205e
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-12-11 19:59:55 +0100

    feat(frontend): improve catalog item visuals (#395)
    * fix(frontend): image url not defined in catalog
    
    Signed-off-by: Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
    
    * fix(frontend): add default value to useResult in catalog
    
    Signed-off-by: Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
    
    * feat(frontend): add missing image and truncate shortDesc in catalog item
    
    Signed-off-by: Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
    
    * refactor(frontend): remove unused code from useCatalog
    
    Signed-off-by: Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
    
    * feat(frontend): add badges to catalog items
    
    Signed-off-by: Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
    
    * feat(frontend): modify shortDesc to text-subtitle3
    
    Signed-off-by: Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>

--- frontend/src/components/catalog/CatalogItem.vue
+++ frontend/src/components/catalog/CatalogItem.vue
@@ -1,42 +1,68 @@
 <template>
-  <router-link
-    :to="{name:'CatalogItemDetail',params:{
-      itemId:props.item.id,
-    }}"
+  <q-card
+    style="width: 100%; max-width: 250px"
   >
-    <q-card
-      style="width: 100%; max-width: 250px"
-    >
-      <q-img
-        :src="SERVER_URL+props.item.images[0].path"
-        fit="scale-down"
-        loading="lazy"
-        style="max-height: 250px"
+    <q-card-section>
+      <router-link
+        :to="{name:'CatalogItemDetail',params:{
+          itemId:props.item.id,
+        }}"
+        class="no-decoration justify-center"
       >
+        <q-img
+          v-if="props.item.images[0]"
+          :src="SERVER_URL + props.item.images[0].path"
+          fit="scale-down"
+          loading="lazy"
+          style="max-height: 250px"
+        />
+        <q-img
+          v-else
+          fit="scale-down"
+          loading="lazy"
+          placeholder-src="~/assets/missing-image.png"
+          src="~/assets/missing-image.png"
+          style="max-height: 250px"
+        />
+      </router-link>
+      <div class="justify-around">
         <q-badge
-          color="purple"
-          rounded
+          class="float-left"
+          color="blue"
         >
+          <q-tooltip>
+            {{ $t('catalog.itemDetail.item.numberOfItemsForExchange') }}
+          </q-tooltip>
           <q-icon
-            color="white"
-            name="swap_horiz"
-          >
-            <q-tooltip>{{ $t('catalog.item.badge.available_for_exchange') }}</q-tooltip>
-          </q-icon>
-          {{ item.numberForExchange }}
+            left
+            name="fas fa-exchange-alt"
+          />
+          {{ props.item.numberForExchange }}
         </q-badge>
-      </q-img>
-      <q-card-section />
-      <q-card-section>
-        <div class="text-h6">
-          {{ props.item.name }}
-        </div>
-        <div class="text-subtitle2">
-          {{ props.item.shortDesc }}
-        </div>
-      </q-card-section>
-    </q-card>
-  </router-link>
+        <q-badge
+          class="float-right q-mr-5"
+          color="green"
+        >
+          <q-tooltip>
+            {{ $t('catalog.itemDetail.item.numberOfItemsInCollection') }}
+          </q-tooltip>
+          {{ props.item.numberInCollection }}
+          <q-icon
+            name="fas fa-layer-group"
+            right
+          />
+        </q-badge>
+      </div>
+    </q-card-section>
+    <q-card-section>
+      <div class="text-h6">
+        {{ props.item.name }}
+      </div>
+      <div class="text-subtitle3 ellipsis-3-lines">
+        {{ props.item.shortDesc }}
+      </div>
+    </q-card-section>
+  </q-card>
 </template>
 <script lang="ts" setup>
 import { CatalogItem } from 'src/composables/useCatalog';
@@ -49,3 +75,9 @@ interface Props {
 const props = defineProps<Props>();
 
 </script>
+
+<style scoped>
+.no-decoration{
+  text-decoration: none;
+}
+</style>
