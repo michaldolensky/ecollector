51cc53b3a24ba120be7854004daebe579fbcd6fd
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-12-11 19:36:00 +0100

    refactor(frontend): remove unused code from useCatalog
    Signed-off-by: Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>

--- frontend/src/composables/useCatalog.ts
+++ frontend/src/composables/useCatalog.ts
@@ -1,63 +1,17 @@
-import { Category, Image } from 'src/apollo/composition-functions';
+import { Image } from 'src/apollo/composition-functions';
 
 export interface CatalogCategory {
-  id:number
-  name:string
-  updatedAt:Date
-  createdAt:Date
-}
-
-export interface CategoryData {
-  categories: CatalogCategory[]
-}
-
-export interface CatalogState{
-  currentCategory:CatalogCategory|null
-  categories:Category[]|null
-  loading:boolean
-}
-
-export interface CatalogCategoryVars {
-  siteId: number;
+  id: number
+  name: string
+  updatedAt: Date
+  createdAt: Date
 }
 
 export interface CatalogItem {
-  id:number
-  name:string
-  shortDesc:string
-  updatedAt:Date
-  createdAt:Date
-  images:Image[]
+  id: number
+  name: string
+  shortDesc: string
+  updatedAt: Date
+  createdAt: Date
+  images: Image[]
 }
-
-export interface ItemData {
-  items: CatalogItem[]
-}
-
-export interface CatalogItemVars {
-  siteId: number;
-  categoryId: number;
-}
-
-const useCatalog = () => {
-  // const { currentSiteId } = useSites();
-  //
-  // // eslint-disable-next-line max-len
-  // const { onResult, loading: load } = useGetCatalogueCategoriesQuery({
-  //   siteId: currentSiteId.value,
-  // });
-  //
-  // onResult((result) => {
-  //   CatalogStateData.categories = result.data.categories;
-  // });
-  //
-  // const catalogState: CatalogState = {
-  //   currentCategory: CatalogStateData.currentCategory,
-  //   categories: CatalogStateData.categories,
-  //   loading: load.value,
-  // };
-
-  // return catalogState;
-};
-
-export default useCatalog;
