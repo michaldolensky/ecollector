7838280c752aef31cdb776bf6aa3e5ee5e791276
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-12-23 21:59:51 +0100

    feat(api): add itemParameters resolver

--- api/src/parameters/parameters.module.ts
+++ api/src/parameters/parameters.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItemParameter } from './entities/ItemParameter.entity';
 import { Parameter } from './entities/parameter.entity';
+import { ItemParametersResolver } from './itemParameters.resolver';
 import { ParametersResolver } from './parameters.resolver';
 import { ParametersService } from './parameters.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Parameter, ItemParameter])],
-  providers: [ParametersResolver, ParametersService],
+  providers: [ParametersResolver, ItemParametersResolver, ParametersService],
   exports: [ParametersService],
 })
 export class ParametersModule {}
