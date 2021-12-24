9933517cac7b2badd06ee10226377c63d2669e26
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-12-24 01:20:20 +0100

    refactor(api): temp remove itemParameters resolver

--- api/src/parameters/parameters.module.ts
+++ api/src/parameters/parameters.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItemParameter } from './entities/itemParameter.entity';
 import { Parameter } from './entities/parameter.entity';
-import { ItemParametersResolver } from './itemParameters.resolver';
 import { ParametersResolver } from './parameters.resolver';
 import { ParametersService } from './parameters.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Parameter, ItemParameter])],
-  providers: [ParametersResolver, ItemParametersResolver, ParametersService],
+  providers: [ParametersResolver, ParametersService],
   exports: [ParametersService],
 })
 export class ParametersModule {}
