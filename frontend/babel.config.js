29dcd351dee92ef65c2610e7cdff42c740813bd3
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2021-12-30 00:35:56 +0100

    feat(frontend): init storybook (#431)

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -7,9 +7,14 @@ module.exports = api => {
         '@quasar/babel-preset-app',
         api.caller(caller => caller && caller.target === 'node')
           ? { targets: { node: 'current' } }
-          : {}
-      ]
-    ]
-  }
-}
+          : {},
+      ],
+    ],
+    plugins: [
+      ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      ['@babel/plugin-proposal-private-methods', { loose: true }],
+    ],
+  };
+};
 
