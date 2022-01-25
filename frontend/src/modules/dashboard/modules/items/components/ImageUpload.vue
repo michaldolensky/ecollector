68161474f744d4b7a2ac3ad93fb71d607250e916
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2022-01-25 19:39:46 +0100

    feat(frontend): refine ImageUpload

--- frontend/src/modules/dashboard/modules/items/components/ImageUpload.vue
+++ frontend/src/modules/dashboard/modules/items/components/ImageUpload.vue
@@ -1,48 +1,59 @@
 <script lang="ts" setup>
 import { QFile } from 'quasar';
+import { useRouteParams } from 'src/composables/useRoute';
 import { useUploadFileMutation } from 'src/modules/dashboard/modules/items/graphql/imageDashboard.operations';
-import { computed, ref } from 'vue';
+import { ref } from 'vue';
 
+const { itemId, siteId } = useRouteParams();
+
+interface Props{
+  disabled: boolean;
+}
+const props = defineProps<Props>();
 const files = ref<File[]>([]);
-const uploading = ref(null);
-
-const isUploading = computed(() => uploading.value !== null);
-const canUpload = computed(() => files.value !== null);
 
 const { mutate: uploadFile } = useUploadFileMutation({});
 
 const upload = () => {
   if (files.value.length > 0) {
     void uploadFile({
-
       files: files.value,
+      siteId: siteId.value,
+      itemId: itemId.value,
 
     }).then((value) => console.log(value));
   }
 };
+const getImageSrc = (file:File) => URL.createObjectURL(file);
 
 </script>
 
 <template>
-  <div class="q-pa-md column items-start q-gutter-y-md">
-    {{ files }}
-    {{ files?.modelValue }}
-    <q-file
-      v-model="files"
-      :clearable="!isUploading"
-      label="Pick files"
-      multiple
-      outlined
-      style="max-width: 400px"
-    />
-    <q-btn
-      :disable="!canUpload"
-      :loading="isUploading"
-      color="primary"
-      dense
-      icon="cloud_upload"
-      round
-      @click="upload"
-    />
-  </div>
+  <q-file
+    v-model="files"
+    :disabled="props.disabled"
+    accept="image/*"
+    counter
+    hint="test"
+    max-file-size="5mb"
+    multiple
+    outlined
+    stack-label="Upload"
+    style="width: 100%;"
+    @update:model-value="upload"
+  >
+    <template #file="{ file }">
+      <div>
+        <q-img
+          :src="getImageSrc(file)"
+          fit="contain"
+          height="100px"
+          width="100px"
+        />
+        <div class="text-caption">
+          {{ file.name }}
+        </div>
+      </div>
+    </template>
+  </q-file>
 </template>
