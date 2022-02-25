ed84583bd33b17aefa5258d7ff80759231504fab
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2022-02-25 21:04:17 +0100

    chore(frontend): update .eslintrc.cjs

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,143 +1,69 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
-  // https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
-  // This option interrupts the configuration hierarchy at this file
-  // Remove this if you have an higher level ESLint config file (it usually happens into a monorepos)
   root: true,
 
-  // https://eslint.vuejs.org/user-guide/#how-to-use-custom-parser
-  // Must use parserOptions instead of "parser" to allow vue-eslint-parser to keep working
-  // `parser: 'vue-eslint-parser'` is already included with any 'plugin:vue/**' config and should be omitted
-  parserOptions: {
-    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/parser#configuration
-    // https://github.com/TypeStrong/fork-ts-checker-webpack-plugin#eslint
-    // Needed to make the parser take into account 'vue' files
-    extraFileExtensions: ['.vue'],
-    parser: require.resolve('@typescript-eslint/parser'),
-    project: require.resolve('./tsconfig.json'),
-    tsconfigRootDir: __dirname,
-    ecmaVersion: 2022, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module' // Allows for the use of imports
-  },
-
-  env: {
-    browser: true,
-    "vue/setup-compiler-macros": true
-  },
-
-  // Rules order is important, please avoid shuffling them
   extends: [
-    // Base ESLint recommended rules
-    'eslint:recommended',
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
 
-    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#usage
-    // ESLint typescript rules
-    'plugin:@typescript-eslint/recommended',
-    // consider disabling this class of rules if linting takes too long
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    "@vue/eslint-config-typescript/recommended",
 
-    // Uncomment any of the lines below to choose desired strictness,
-    // but leave only one uncommented!
-    // See https://eslint.vuejs.org/rules/#available-rules
-    // 'plugin:vue/vue3-essential', // Priority A: Essential (Error Prevention)
-    // 'plugin:vue/vue3-strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
-    'plugin:vue/vue3-recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
     "@vue/eslint-config-prettier",
-    'plugin:@intlify/vue-i18n/recommended',
-  ],
-
-  plugins: [
-    // required to apply rules which need type information
-    '@typescript-eslint',
-
-    // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-file
-    // required to lint *.vue files
-    'vue',
-
+    "plugin:@intlify/vue-i18n/recommended",
   ],
 
   globals: {
-    ga: 'readonly', // Google Analytics
-    cordova: 'readonly',
-    __statics: 'readonly',
-    __QUASAR_SSR__: 'readonly',
-    __QUASAR_SSR_SERVER__: 'readonly',
-    __QUASAR_SSR_CLIENT__: 'readonly',
-    __QUASAR_SSR_PWA__: 'readonly',
-    process: 'readonly',
-    Capacitor: 'readonly',
-    chrome: 'readonly',
-    //https://eslint.vuejs.org/user-guide/#faq
-    defineProps: "readonly",
-    defineEmits: "readonly",
-    defineExpose: "readonly",
-    withDefaults: "readonly"
+    ga: "readonly", // Google Analytics
+    cordova: "readonly",
+    __statics: "readonly",
+    __QUASAR_SSR__: "readonly",
+    __QUASAR_SSR_SERVER__: "readonly",
+    __QUASAR_SSR_CLIENT__: "readonly",
+    __QUASAR_SSR_PWA__: "readonly",
+    process: "readonly",
+    Capacitor: "readonly",
+    chrome: "readonly",
   },
-
+  env: {
+    browser: true,
+    "vue/setup-compiler-macros": true,
+  },
   // add your custom rules here
   rules: {
-    'no-param-reassign': 'off',
-    'no-void': 'off',
-    'no-nested-ternary': 'off',
-    'max-classes-per-file': 'off',
-
-    'import/first': 'off',
-    'import/named': 'error',
-    'import/namespace': 'error',
-    'import/default': 'error',
-    'import/export': 'error',
-    'import/extensions': 'off',
-    'import/no-unresolved': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'import/prefer-default-export': 'off',
-    'prefer-promise-reject-errors': 'off',
-
-    "no-shadow": "off",
-    "@typescript-eslint/no-shadow": ["error"],
-    "no-unused-vars": "off",
-
+    "prefer-promise-reject-errors": "off",
     //i18n
-    '@intlify/vue-i18n/no-unused-keys': 'warn',
-    '@intlify/vue-i18n/no-missing-keys-in-other-locales': 'warn',
-
-    // TypeScript
-    quotes: ['warn', 'single', {avoidEscape: true}],
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    "@intlify/vue-i18n/no-unused-keys": "warn",
+    "@intlify/vue-i18n/no-missing-keys-in-other-locales": "warn",
 
     // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    '@typescript-eslint/no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-    'max-len': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
 
-    "vue/component-tags-order": ["error", {
-      "order": [ "script", "template", "style" ]
-    }]
+    "vue/component-tags-order": [
+      "error",
+      {
+        order: ["script", "template", "style"],
+      },
+    ],
   },
-  "overrides": [
+  overrides: [
     {
-      "files": ["./src/pages/Error-401.vue", "./src/pages/Error-404.vue"],
-      "rules": {
-        "@intlify/vue-i18n/no-raw-text": "off"
-      }
-    },
-    {
-      "files": ["./src/pages/site/catalog/CatalogItemDetail.vue"],
-      "rules": {
-        "vue/no-v-html": "off"
-      }
-    },
-    {
-      files: ['**/stories/*.*'],
+      files: ["./src/pages/Error-401.vue", "./src/pages/Error-404.vue"],
       rules: {
-        '@typescript-eslint/no-unsafe-assignment': 'off',
-        '@typescript-eslint/no-unsafe-member-access': 'off',
+        "@intlify/vue-i18n/no-raw-text": "off",
+      },
+    },
+    {
+      files: ["./src/modules/catalog/pages/CatalogItemDetail.vue"],
+      rules: {
+        "vue/no-v-html": "off",
       },
     },
   ],
-  "settings": {
+  settings: {
     "vue-i18n": {
-      "localeDir": "./src/i18n/*.json"
-    }
-  }
-}
+      localeDir: "./src/i18n/*.json",
+    },
+  },
+};
