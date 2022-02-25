a405ecfd2e09c994b132a824ccefe100b8b92751
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2022-02-25 23:56:05 +0100

    feat(frontend): convert to urql

--- frontend/src/components/SiteToolbar.vue
+++ frontend/src/components/SiteToolbar.vue
@@ -1,14 +1,18 @@
 <script lang="ts" setup>
-import { useSiteQuery } from "src/modules/account/graphql/sitesDashboard.operations";
+import { useSiteQuery } from "src/modules/account/graphql/sitesDashboard.operations.urql";
 import { useSites } from "src/composables/useSites";
 
 const { currentSiteId } = useSites();
-const { result } = useSiteQuery({ id: currentSiteId.value });
+const { data } = useSiteQuery({
+  variables: {
+    id: currentSiteId.value,
+  },
+});
 </script>
 <template>
   <q-toolbar inset>
-    <q-toolbar-title v-if="result" shrink>
-      {{ result.site.name }}
+    <q-toolbar-title v-if="data" shrink>
+      {{ data.site.name }}
     </q-toolbar-title>
     <q-separator dark inset spaced="md" vertical />
     <q-tabs align="left">
