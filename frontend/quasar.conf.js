1b4a796e18de21b6fce7748c193d7ae2970c05f6
Michal Dolenský <1257948+michaldolensky@users.noreply.github.com>
2022-03-14 19:18:01 +0100

    refactor(frontend): convert boot pinia to store index

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -27,7 +27,7 @@ module.exports = configure(function (/* ctx */) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: ["i18n", "axios", "pinia", "urql"],
+    boot: ["i18n", "axios", "urql"],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
     css: ["app.scss"],
---
+++ frontend/src/stores/index.ts
@@ -0,0 +1,24 @@
+import { createPinia } from "pinia";
+import { store } from "quasar/wrappers";
+/*
+ * If not building with SSR mode, you can
+ * directly export the Store instantiation;
+ *
+ * The function below can be async too; either use
+ * async/await or return a Promise which resolves
+ * with the Store instance.
+ */
+import router from "src/router";
+import { markRaw } from "vue";
+
+export default store((/* { ssrContext } */) => {
+  const pinia = createPinia();
+
+  // You can add Pinia plugins here
+  // pinia.use(SomePiniaPlugin)
+  pinia.use(({ store }) => {
+    store.router = markRaw(router);
+  });
+
+  return pinia;
+});
